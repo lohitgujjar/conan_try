c035cfabd1dcdbbe154e47d5db096cf28689ba89
Lohit B. Veeranna <l.banagerae.veeranna@ar-lx0234.ar.int>
2021-07-08 01:49:20 +0200

    try

--- hello.cpp
+++ hello.cpp
@@ -5,7 +5,7 @@
 
 void hello() {
     #ifdef NDEBUG
-    std::cout << "Hello World Release!" <<std::endl;
+    // std::cout << "Hello World Release!" <<std::endl;
     #else
     std::cout << "Hello World Debug!" <<std::endl;
     #endif
---
+++ test_package/build/ef44ae3cb0b7943825b49b1b0426c7d3df566f6f/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/l.banagerae.veeranna/Music/lohit_github/test_package/example.cpp" "/home/l.banagerae.veeranna/Music/lohit_github/test_package/build/ef44ae3cb0b7943825b49b1b0426c7d3df566f6f/CMakeFiles/example.dir/example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_GLIBCXX_USE_CXX11_ABI=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/l.banagerae.veeranna/.conan/data/hello/5.1/l.banagerae.veeranna/test/package/66c5327ebdcecae0a01a863939964495fa019a06/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
