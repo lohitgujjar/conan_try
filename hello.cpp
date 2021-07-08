5ee14be2cf2f449d2c5ca3f20690bc27afd53bfb
lohitgujjar <49302063+lohitgujjar@users.noreply.github.com>
2021-07-08 09:50:28 +0200

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -8,7 +8,7 @@ void hello() {
     // std::cout << "Hello World Release!" <<std::endl;
     #else
     //hello there
-    std::cout << "Hello World Debug!" <<std::endl;
+    std::cout << "Hello World Debug-conan testing!" <<std::endl;
     std::cout <<"Hi there- conan testing" <<std::endl;
     #endif
 }
